4083550d0e80981363ab9e6fce1dcc6e72873c3b
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-01-03 02:31:14 +0800

    added served HTML file at myApp.js

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ var app = express();
 console.log("Hello World");
 
 app.get("/",(req,res)=> {
-    res.send("Hello Express");
+    //res.send("Hello Express");
 
-    //res.sendFile(__dirname/views.index.html);
+    res.sendFile(__dirname + "/views/index.html");
 })
 
 
