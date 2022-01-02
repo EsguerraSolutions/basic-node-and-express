d5948e2a483fbb09a18365e2963d76ef1f9e2714
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-01-03 02:58:26 +0800

    served style.css from /public directory at myApp.js

--- myApp.js
+++ myApp.js
@@ -7,6 +7,8 @@ app.get("/",(req,res)=> {
     //res.send("Hello Express");
 
     res.sendFile(__dirname + "/views/index.html");
+
+    app.use("/public",express.static(__dirname + "/public"));
 })
 
 
