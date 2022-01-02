eb1bd0000a43977d7d92fefd00418b8ba58fe5b5
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-01-03 03:14:43 +0800

    fixed json route, added json object at myApp.js

--- myApp.js
+++ myApp.js
@@ -6,9 +6,12 @@ var app = express();
 app.get("/",(req,res)=> {
     //res.send("Hello Express");
 
-    //res.sendFile(__dirname + "/views/index.html");
+    res.sendFile(__dirname + "/views/index.html");
 
-    //app.use("/public",express.static(__dirname + "/public"));
+    app.use("/public",express.static(__dirname + "/public"));
+});
+
+app.get("/json",(req,res)=> {
 
     res.json({"message": "Hello json"});
 })
