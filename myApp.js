f5f76e2eac44f10889bc29005ca1cfb703b98eea
Jonathan Esguerra <EsguerraDesign@gmail.com>
2022-01-04 14:59:02 +0800

    added dotenv process and MESSAGE_STYLE variable for res.json

--- myApp.js
+++ myApp.js
@@ -1,3 +1,6 @@
+
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 
@@ -8,12 +11,19 @@ app.get("/",(req,res)=> {
 
     res.sendFile(__dirname + "/views/index.html");
 
-    app.use("/public",express.static(__dirname + "/public"));
 });
 
-app.get("/json",(req,res)=> {
+app.use("/public",express.static(__dirname + "/public"));
 
-    res.json({"message": "Hello json"});
+
+app.get("/json",(req,res)=> {
+    
+    if (process.env.MESSAGE_STYLE === "uppercase" ) {
+        res.json({"message": "HELLO JSON"});
+    }
+    else {
+        res.json({"message": "Hello json"});
+    }
 })
 
 
